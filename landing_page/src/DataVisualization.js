676b9bd2252cf04eb69ba6347f32e09eb0e72852
Ying <ying1996@gmail.com>
2019-02-01 12:56:21 +0800

    add more skeletons

--- landing_page/src/DataVisualization.js
+++ landing_page/src/DataVisualization.js
@@ -5,7 +5,7 @@ constructor(props) {
   }
 render() {
     const element = (<div>Text from Element</div>)
-    return (<div className="comptext">
+    return (<div className="datavis-div">
     <h3>Data Visualization</h3>
       {this.props.displaytext}
       {element}
