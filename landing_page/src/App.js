3d60de28648940edde3e36952dca0b75eeae2153
Ying <ying1996@gmail.com>
2019-02-01 11:38:56 +0800

    add div

--- landing_page/src/App.js
+++ landing_page/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+		<div></div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
