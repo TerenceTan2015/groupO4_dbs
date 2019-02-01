5272125e3b2a439707fe47c2904e4f2c0af2a1a8
Ying <ying1996@gmail.com>
2019-02-01 12:16:44 +0800

    skeleton codes

--- landing_page/src/App.js
+++ landing_page/src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './FirstComponent'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-		<div></div>
+		<FirstComponent displaytext="Header goes here"/>
+		
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+		  
           <a
             className="App-link"
             href="https://reactjs.org"
